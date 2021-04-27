c493e2f9dae79e909535aa833373e1d609e12111
Sohaib <sohaib.f.bataineh@gmail.com>
2021-04-27 15:50:05 +0300

    27042021

--- lib/AddStudent.dart
+++ lib/AddStudent.dart
@@ -8,18 +8,162 @@ class AddStudent extends StatefulWidget {
 }
 
 class _NameState extends State<AddStudent> {
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('add student'),
         ),
-        body: Padding(
-            child: Center(
-                child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [Text('ID'), TextField()],
-            )),
-            padding: EdgeInsets.fromLTRB(32, 0, 32, 0)));
+        body: Form(
+            key: _formkey,
+            child: Padding(
+                padding:
+                    EdgeInsets.only(top: 8, left: 64, right: 64, bottom: 64),
+                child: ListView(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Id'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'enter id';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'First name'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'enter id';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'M Name'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'enter id';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Last Name'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'enter id';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(labelText: 'National ID'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'enter id';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Grade'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'enter id';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Class'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'enter id';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration:
+                          InputDecoration(labelText: 'Transportation Type'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'enter id';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Parent Name'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'enter id';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                          icon: Icon(Icons.mobile_friendly_sharp),
+                          labelText: 'Mobile'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'enter id';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          icon: Icon(Icons.mail), labelText: 'Email'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'enter id';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {}, child: Text('Add student')),
+                  ],
+                ))));
   }
 }
