ea19836b614620e7c377666b37efb49dba874d7a
Sohaib.Bataineh <sohaib.f.bataineh@gmail.com>
2021-05-03 15:05:31 +0300

    05032021

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/landingpage.dart';
-import 'package:flutter_application_1/login.dart';
 
 void main() => runApp(MyApp());
 
